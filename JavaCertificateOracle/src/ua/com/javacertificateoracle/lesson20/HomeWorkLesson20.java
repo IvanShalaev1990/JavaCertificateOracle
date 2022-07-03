c19402d70f471cb09e7a0f9045edb4aeb34b0fd5
IvanShalaev <ivanshalaev1990@gmail.com>
2022-07-03 17:24:08 +0300

    Add changes to lesson20

--- JavaCertificateOracle/src/ua/com/javacertificateoracle/lesson20/HomeWorkLesson20.java
+++ JavaCertificateOracle/src/ua/com/javacertificateoracle/lesson20/HomeWorkLesson20.java
@@ -3,16 +3,24 @@ package ua.com.javacertificateoracle.lesson20;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class HomeWorkLesson20 {
-    public static void stringSorting(String ... str){
-        List<String> myList = Arrays.asList(str);
-        System.out.println(myList);
+    public static void stringSorting(String ... strArray){
+        ArrayList <String> list = new ArrayList<>();
+        for (String str:
+             strArray) {
+            if(!list.contains(str)){
+                list.add(str);
+            }
+        }
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
 class HomeWorkLesson20Test{
     public static void main(String[] args) {
-        HomeWorkLesson20.stringSorting("Hello", "World", "Rape", "Me");
+        HomeWorkLesson20.stringSorting("Hello", "World", "Rape", "Me", "Hello", "World", "Rape", "Me");
     }
 }
